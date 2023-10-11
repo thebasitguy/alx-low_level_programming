9637cab56e9e9697999ab59a2185093c61744057
thebasitguy <mailbasitgh@gmail.com>
2023-10-10 18:47:35 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabets in lowercase
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char i = 'a';
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+
+}
