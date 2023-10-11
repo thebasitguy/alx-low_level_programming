51e67c4743baa94ed092d51f2875098f0373aac6
thebasitguy <mailbasitgh@gmail.com>
2023-10-10 21:24:31 -0700

    prints the n times table

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,11 +6,12 @@
  */
 int print_last_digit(int n)
 {
-	if (n < 0)
+	int print_last_digit(int n)
 	{
-		n *= -1;
-	}
-	_putchar('0' + (n % 10));
+		if (n < 0)
+			n *= -1;
+		_putchar('0' + (n % 10));
 
 	return (n % 10);
+	}
 }
