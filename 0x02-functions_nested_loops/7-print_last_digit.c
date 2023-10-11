6697372f247854c6ab4780e5e18623a192b7ef98
thebasitguy <mailbasitgh@gmail.com>
2023-10-10 22:17:14 -0700

    prints the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,12 +6,10 @@
  */
 int print_last_digit(int n)
 {
-	int print_last_digit(int n)
-	{
-		if (n < 0)
-			n *= -1;
-		_putchar('0' + (n % 10));
+	if (n < 0)
+		n *= -1;
+
+	_putchar('0' + (n % 10));
 
 	return (n % 10);
-	}
 }
