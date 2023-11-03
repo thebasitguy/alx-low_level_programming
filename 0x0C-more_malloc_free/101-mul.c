9eb6534d4f4bad8d8c335c395d25990ad391d111
thebasitguy <mailbasitgh@gmail.com>
2023-11-02 22:36:19 -0700

    a program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,59 +1,126 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include "main.h"
+
+/* helper functions */
+#define _atoi(c) (c - '0')
+#define _itoa(i) ((i % 10) + '0')
+#define isdigit(c) ((c >= '0' && c <= '9') ? 1 : 0)
+
+void *_memset(void *s, int c, unsigned int n);
+void print_err_msg(void);
+unsigned int _strlen(const char *s);
 
 /**
- * is_positive_number - Checks if a string is a positive number.
- * @str: The input string to check.
- * Return: 1 if str is a positive number, 0 otherwise.
- */
-
-int is_positive_number(const char *str)
-{
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (!isdigit(str[i]))
-		{
-			return (0);
-		}
-	}
-
-	return (1);
-}
-
-/**
- * main - Multiplies two positive numbers.
- * @argc: The number of command-line arguments.
- * @argv: An array of command-line argument strings.
- * Return: 0 on success, 98 on incorrect number of arguments or invalid input.
+ * main - multiplies two numbers provided as command line arguments.
+ * @argc: the number of command line arguments.
+ * @argv: an array containing the command line arguments. Exactly two
+ * arguments are required.
+ *
+ * Return: 0 on success
  */
 
 int main(int argc, char *argv[])
 {
-	int num1;
-	int num2;
-	int result;
+	char *op1, *op2;
+	int op1_len, op2_len, result_len;
+	int start, product, carry, i, j;
+	char *result;
 
 	if (argc != 3)
+		print_err_msg();
+
+	op1 = argv[1];
+	op2 = argv[2];
+
+	op1_len = _strlen(op1);
+	op2_len = _strlen(op2);
+
+	result_len = op1_len + op2_len;
+
+	result = malloc(sizeof(char) * (result_len + 1));
+
+	if (result == NULL)
+		print_err_msg();
+
+	_memset(result, '0', result_len);
+	result[result_len] = '\0';
+
+	for (i = op1_len - 1; i >= 0; i--)
 	{
-		printf("Error\n");
-		return (98);
+		for (j = op2_len - 1; j >= 0; j--)
+		{
+			if (!isdigit(op1[i]) && !isdigit(op2[j]))
+			{
+				free(result);
+				print_err_msg();
+			}
+
+			product = _atoi(op1[i]) * _atoi(op2[j]);
+			carry = _atoi(result[i + j + 1]) + product;
+
+			result[i + j + 1] = _itoa(carry);
+			result[i + j] = (_atoi(result[i + j]) + _itoa(carry / 10));
+		}
 	}
 
-	if (!is_positive_number(argv[1]) || !is_positive_number(argv[2]))
-	{
-		printf("Error\n");
-		return (98);
-	}
+	start = 0;
+	while (start < result_len && result[start] == '0')
+		start++;
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	result = num1 * num2;
+	if (start == result_len)
+		puts("0");
+	else
+		printf("%s\n", result + start);
 
-	printf("%d\n", result);
+	free(result);
 
 	return (0);
+}
+
+/**
+ * print_err_msg - print error and exit with status code 98
+ */
+
+void print_err_msg(void)
+{
+	puts("Error");
+	exit(98);
+}
+
+/**
+ * _strlen - get the length of a string
+ * @s: string
+ *
+ * Return: length of string @s
+ */
+
+unsigned int _strlen(const char *s)
+{
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+		;
+
+	return (len);
+}
+
+/**
+ * _memset - fill memory with constant byte
+ * @s: string buffer
+ * @c: byte to write
+ * @n: number of bytes to write
+ *
+ * Return: a pointer to memory area @s
+ */
+
+void *_memset(void *s, int c, unsigned int n)
+{
+	unsigned int i;
+	char *tmp = (char *) s;
+
+	for (i = 0; i < n; i++)
+		tmp[i] = c;
+	return (s);
 }
 
