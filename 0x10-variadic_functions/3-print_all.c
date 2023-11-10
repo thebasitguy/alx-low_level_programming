fde0039b84917ea85c75d21c68cd805b11b8ea76
thebasitguy <mailbasitgh@gmail.com>
2023-11-09 16:55:35 -0800

    a function that prints anything

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,52 @@
+#include "variadic_functions.h"
+#define is_fmt(c) (c == 'c' || c == 'i' || c == 's' || c == 'f')
+
+/**
+ * print_all - function that prints anything
+ *
+ * @format: list of argument types passed to the function
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	char *str, *tmp_fmt;
+
+	va_start(args, format);
+
+	tmp_fmt = (char *)format;
+
+	while (tmp_fmt && *tmp_fmt != '\0')
+	{
+		switch (*tmp_fmt)
+		{
+		case 'c':
+			putchar(va_arg(args, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(args, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(args, double));
+			break;
+		case 's':
+			str = va_arg(args, char *);
+			if (!str)
+				str = "(nil)";
+			printf("%s", str);
+			break;
+		default:
+			++tmp_fmt;
+			continue;
+		}
+
+		if (is_fmt(*tmp_fmt) && *(tmp_fmt + 1) != '\0')
+			printf(", ");
+
+		++tmp_fmt;
+	}
+
+	putchar('\n');
+	va_end(args);
+}
+
