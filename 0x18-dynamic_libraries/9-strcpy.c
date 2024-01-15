ed019b7a845f10619e2eb52527346af2b3415cc0
thebasitguy <mailbasitgh@gmail.com>
2024-01-15 15:59:07 -0800

    drnamic library

---
+++ 0x18-dynamic_libraries/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * char *_strcpy - a function that copies the string pointed to by src
+ * @dest: copy destination
+ * @src: copy source
+ * Return: string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int a = 0;
+	int b = 0;
+
+	while (*(src + a) != '\0')
+	{
+		a++;
+	}
+	for ( ; b < a ; b++)
+	{
+		dest[b] = src[b];
+	}
+	dest[a] = '\0';
+	return (dest);
+}
