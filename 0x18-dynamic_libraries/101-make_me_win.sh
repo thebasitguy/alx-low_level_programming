f31678373e0cf3d476f9f4390a797b973dfd542d
thebasitguy <mailbasitgh@gmail.com>
2024-01-15 19:45:02 -0800

    final one

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/thebasitguy/alx-low_level_programming/main/0x18-dynamic_libraries/libhack.so
-export LD_PRELOAD="$PWD/../libhack.so"
+wget -P .. https://raw.githubusercontent.com/thebasitguy/alx-low_level_programming/main/0x18-dynamic_libraries/libcomplete.so
+export LD_PRELOAD="$PWD/../libcomplete.so"
