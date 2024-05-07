ba7dbb09c2348ba39079e6e2ebe7ad40d2b24329
thebasitguy <mailbasitgh@gmail.com>
2024-05-07 06:17:54 -0700

    a function that searches for a value in a sorted array of integers using the Binary search algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -4,12 +4,12 @@
   * linear_search - Searches for a value in an array
   *                 of integers using linear search.
   *
-  * @array: A pointer to the first element of the array to search.
-  * @size: The number of elements in the array.
-  * @value: The value to search for.
+  * @array: Pointer to the first element of the array to search.
+  * @size: Number of elements in the array.
+  * @value: Value to search for.
   *
-  * Return: If the value is not present or the array is NULL, -1.
-  *         else, the first index where the value is located.
+  * Return: -1 if the value is not present or the array is NULL.
+  *         Else, the first index where the value is located.
   *
   * Description: Prints a value every time it is compared in the array.
   */
